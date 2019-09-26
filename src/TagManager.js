1a3a8bb09acbf87da7623f92b72ffc00688b55d7
para7 <cometpara7@gmail.com>
2019-09-26 21:24:24 +0900

    progress

--- src/TagManager.js
+++ src/TagManager.js
@@ -11,7 +11,7 @@ TagDB.secondaryTags = new Array();
 
 //データ初期化
 (function() {
-    // console.log("call once");
+    console.log("Tag init");
 
 
 }())
@@ -19,21 +19,71 @@ TagDB.secondaryTags = new Array();
 //データ操作メソッド
 TagDB.AddTag = function() {
     let id = event.target.id;
-    console.log(id);
+    // console.log(id);
 
-    let db;
+    let db, input;
 
     if (id === "primaryadd") {
-        console.log("primary");
+        // console.log("primary");
         db = TagDB.primaryTags;
+        input = document.getElementById("primaryinput");
     } else if (id === "secondaryadd") {
-        console.log("secondary");
+        // console.log("secondary");
+        db = TagDB.secondaryTags;
+        input = document.getElementById("secondaryinput");
     } else {
-        console.log("error");
+        console.log("AddTag error");
         return;
     }
+
+    let text = input.value;
+
+    if (text === "") {
+        console.log("empty");
+        return;
+    }
+
+    db.push(text);
+
+    TagDB.UpdateDisplay();
 }
 
+TagDB.UpdateDisplay = function() {
+
+
+    console.log(document.getElementById("ptest"));
+
+    let tags = document.getElementById("primarytags");
+    console.log(tags);
+    let children = tags.children;
+    console.log(children);
+    console.log(tags.firstChild);
+
+    // イベントの解除
+    // for (let i = 0, l = children.length; i < l; ++i) {
+    //     children[i].removeEventListener('click', SetClipboard);
+    // }
+
+    // 要素のリセット
+    while (tags.firstChild) {
+        tags.removeChild(tags.firstChild);
+    }
+
+    for (let k = 0, lt = TagDB.primaryTags.length; k < lt; ++k) {
+
+        let p = document.createElement('p');
+        tags.insertAdjacentElement('beforeend', p);
+
+        const text = document.createTextNode(TagDB.primaryTags[k]);
+        p.appendChild(text);
+        const button = document.createElement('button');
+        const bttext = document.createTextNode('削除');
+        button.appendChild(bttext);
+        p.appendChild(button);
+
+        tags.insertAdjacentElement('beforeend', p);
+    }
+}
 
 document.getElementById("primaryadd").addEventListener('click', TagDB.AddTag);
 document.getElementById("secondaryadd").addEventListener('click', TagDB.AddTag);
