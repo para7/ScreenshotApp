46777c3a35f3bf8e312b33aaa91d194859028340
para7 <cometpara7@gmail.com>
2019-10-15 17:11:58 +0900

    format

--- src/main.js
+++ src/main.js
@@ -4,53 +4,51 @@ const electron = require('electron');
 const { app, BrowserWindow, ipcMain, Menu } = electron;
 
 app.on('window-all-closed', function() {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 let mainWindow = null;
 
 app.on('ready', () => {
-    // ブラウザ(Chromium)の起動, 初期画面のロード
-    mainWindow = new BrowserWindow({
-        width: 1400,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  // ブラウザ(Chromium)の起動, 初期画面のロード
+  mainWindow = new BrowserWindow({
+    width : 1400,
+    height : 600,
+    webPreferences : {
+      nodeIntegration : true
+    }
+  });
 
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
-    mainWindow.openDevTools();
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.openDevTools();
 
-    mainWindow.on('closed', () => app.quit());
+  mainWindow.on('closed', () => app.quit());
 
-    // Menu
-    const mainMenu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(mainMenu);
+  // Menu
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 
 // Menu
-let menuTemplate = [{
-    label: "Menu",
-    submenu: [{
-            label: "AddScreenshot",
-            click: () => {
-
-                mainWindow.loadURL('file://' + __dirname + '/mkdb.html')
-            }
-        },
-        {
-            label: "Quit",
-            accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
-            click() {
-                app.quit();
-            }
-        }
-    ]
-}];
+let menuTemplate = [ {
+  label : "Menu",
+  submenu : [ {
+               label : "AddScreenshot",
+               click : () => {
+                 mainWindow.loadURL('file://' + __dirname + '/mkdb.html')
+               }
+             },
+    {
+      label : "Quit",
+      accelerator : process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
+      click() {
+        app.quit();
+      }
+    } ]
+} ];
 
 if (process.platform == 'darwin') {
-    menuTemplate.unshift({ label: '' });
+  menuTemplate.unshift({ label : '' });
 }
