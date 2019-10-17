88038055ef36f901c76801373074275aeaab3b92
para7 <cometpara7@gmail.com>
2019-10-18 01:39:55 +0900

    delete tag

--- src/TagManager.js
+++ src/TagManager.js
@@ -1,109 +1,104 @@
 'use strict';
 
 if (typeof TagDB === 'undefined') {
-  var TagDB = {};
+    var TagDB = {};
 }
-
-//データセット
-
-TagDB.Tags = new Array();
-
-//データ初期化
-(function() { console.log('Tag init'); }())
 
 //データ操作メソッド
 TagDB.AddTag = function() {
-  let id = event.target.id;
+    let id = event.target.id;
 
-  let db, input;
+    let db, input;
 
-  db = TagDB.Tags;
-  input = document.getElementById('dbinput');
+    db = TagDB.Tags;
+    input = document.getElementById('dbinput');
 
-  let text = input.value;
+    let text = input.value;
 
-  if (text === '') {
-    console.log('empty');
-    return;
-  }
+    if (text === '') {
+        console.log('empty');
+        return;
+    }
 
-  db.push(text);
+    db.push(text);
 
-  TagDB.UpdateDisplay();
+    TagDB.UpdateDisplay();
 
-  input.value = '';
+    input.value = '';
 };
 
 TagDB.DeleteTag = function() {
-  let current = event.currentTarget.parentNode;
+    const tags = document.taglist.tag;
 
-  let text = current.firstChild.textContent;
-
-  let db = TagDB.Tags;
-
-  //消す
-  const index = db.findIndex((v) => v === text);
-  db.splice(index, 1);
-
-  TagDB.UpdateDisplay();
+    for (let i = 0; i < tags.length; i++) {
+        if (tags[i].checked) { //(color1[i].checked === true)と同じ
+            const index = TagDB.Tags.findIndex((v) => v === tags[i].value);
+            TagDB.Tags.splice(index, 1);
+        }
+    }
+    TagDB.UpdateDisplay();
 };
 
 TagDB.UpdateDisplay = function() {
-  let tags = document.getElementById('dbtags');
-  let children = tags.children;
+    let tags = document.getElementById('dbtags');
+    let children = tags.children;
 
-  // イベントの解除
-  for (let i = 0, l = children.length; i < l; ++i) {
-    children[i].removeEventListener('click', TagDB.DeleteTag);
-  }
+    // イベントの解除
+    for (let i = 0, l = children.length; i < l; ++i) {
+        children[i].removeEventListener('click', TagDB.DeleteTag);
+    }
 
-  // 要素のリセット
-  while (tags.firstChild) {
-    tags.removeChild(tags.firstChild);
-  }
+    // 要素のリセット
+    while (tags.firstChild) {
+        tags.removeChild(tags.firstChild);
+    }
 
-  let form = document.createElement('form');
-  form.name = "taglist";
-  tags.appendChild(form);
+    let form = document.createElement('form');
+    form.name = "taglist";
+    tags.appendChild(form);
 
-  //タグ一覧の更新
-  for (let k = 0, lt = TagDB.Tags.length; k < lt; ++k) {
-    //全体のdiv
-    let div = document.createElement('div');
+    //タグ一覧の更新
+    for (let k = 0, lt = TagDB.Tags.length; k < lt; ++k) {
+        //全体のdiv
+        let div = document.createElement('div');
 
-    //文字
-    let span = document.createElement('span');
-    const text = document.createTextNode(TagDB.Tags[k]);
-    span.appendChild(text);
+        //文字
+        let span = document.createElement('span');
+        const text = document.createTextNode(TagDB.Tags[k]);
+        span.appendChild(text);
 
-    //チェックボックス
-    let cbox = document.createElement('input');
-    cbox.type = 'checkbox';
-    cbox.value = TagDB.Tags[k];
+        //チェックボックス
+        let cbox = document.createElement('input');
+        cbox.type = 'checkbox';
+        cbox.value = TagDB.Tags[k];
+        cbox.name = 'tag';
 
-    //ボタンをセット
-    const button = document.createElement('button');
-    const bttext = document.createTextNode('削 除');
-    button.appendChild(bttext);
-    button.addEventListener('click', TagDB.DeleteTag);
+        //チェックボックスを追加
+        div.appendChild(cbox);
+        //タグの中身を追加
+        div.appendChild(span);
 
-    //チェックボックスを追加
-    div.appendChild(cbox);
-    //タグの中身を追加
-    div.appendChild(span);
-    //削除ボタンを追加
-    div.appendChild(button);
+        console.log(div);
 
-    console.log(div);
-
-    // form.appendChild(div);
-    form.appendChild(cbox);
-  }
-  console.log(form);
+        // form.appendChild(div);
+        form.appendChild(div);
+    }
+    console.log(form);
 };
 
-document.getElementById('dbadd')
-  .addEventListener('click', TagDB.AddTag);
+//データセット
+TagDB.Tags = new Array();
+
+//データ初期化
+(function() {
+console.log('Tag init');
+TagDB.Tags = new Array("ミリシタ", "七尾百合子", "高山紗代子");
+TagDB.UpdateDisplay();
+}())
+
+document.getElementById('dbadd').addEventListener('click', TagDB.AddTag);
+
+document.getElementById('deletetag').addEventListener('click', TagDB.DeleteTag);
 
 /*
 
